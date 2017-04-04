124a82a2633ea9a97a0ccfa4ed210f3d695d0587
Etienne <etienne.vbg@hotmail.fr>
2017-04-04 23:13:54 +0200

    Add a class for abilities

--- characters.py
+++ characters.py
@@ -1,24 +1,47 @@
 """Here we are going to create several classes
 	of characters available for the player,
 	and also every work the player can do"""
+
+
+class Ability():
+	def __init__(self,strength=0,agility=0,endurance=0,intelligence=0,will=0,luck=0):
+		self.strength=strength
+		self.agility=agility
+		self.endurance=endurance
+		self.intelligence=intelligence
+		self.will=will
+		self.luck=luck
+	def change_ability_strength(self,value):
+		self.strength+=value
+	def change_ability_agility(self,value):
+		self.agility+=value
+	def change_ability_endurance(self,value):
+		self.endurance+=value
+	def change_ability_intelligence(self,value):
+		self.intelligence+=value
+	def change_ability_will(self,value):
+		self.will+=value
+	def change_ability_luck(self,value):
+		self.luck+=value
 	
 
-class Character():
+class Character(Ability):
 	def __init__ (self,life_points,mana_points,experience,
-					level,work,style,stats):
+					level,work,style):
 		self.life_points=life_points
 		self.mana_points=mana_points
 		self.level=level
 		self.experience=experience
 		self.work=work
 		self.style=style
-		self.stats=stats
+		Ability.__init__(self,0,0,0,0,0,0)
 
 	def show_caracs(self):
 		return [self.life_points,self.mana_points,
 					self.level,self.experience, self.work]
-	def show_stats(self):
-		return self.stats
+	def show_abilities(self):
+		return [self.strength,self.agility,self.endurance,
+				self.intelligence,self.will,self.luck]
 
 	def lose_lp(self,nbr):
 		new_nbr_of_life_points=self.life_points-nbr
