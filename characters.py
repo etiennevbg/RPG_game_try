375f7bd5e6746ed0c04ec732f1284f1f00964e5c
Etienne <etienne.vbg@hotmail.fr>
2017-04-04 22:23:52 +0200

    Add features to the character class

--- characters.py
+++ characters.py
@@ -4,20 +4,36 @@
 	
 
 class Character():
-	def __init__ (self,life_points,mana_points,level,
-					work,style,stats):
+	def __init__ (self,life_points,mana_points,experience,
+					level,work,style,stats):
 		self.life_points=life_points
 		self.mana_points=mana_points
 		self.level=level
+		self.experience=experience
 		self.work=work
 		self.style=style
 		self.stats=stats
 
 	def show_caracs(self):
 		return [self.life_points,self.mana_points,
-					self.level,self.work]
-
+					self.level,self.experience, self.work]
 	def show_stats(self):
 		return self.stats
 
-
+	def lose_lp(self,nbr):
+		new_nbr_of_life_points=self.life_points-nbr
+		self.life_points=new_nbr_of_life_points
+	def gain_lp(self,nbr):
+		new_nbr_of_life_points=self.life_points+nbr
+		self.life_points=new_nbr_of_life_points
+	def lose_mana(self,nbr):
+		new_nbr_of_mana_points=self.mana_points-nbr
+		self.mana_points=new_nbr_of_mana_points
+	def gain_mana(self,nbr):
+		new_nbr_of_mana_points=self.mana_points+nbr
+		self.mana_points=new_nbr_of_mana_points
+	def gain_exp(self,value):
+		self.experience+=value
+	def level_up(self, nbr=1):
+		self.level+=nbr
+	
