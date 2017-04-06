5c31141e663e83e6139e9d23742ade213a544971
Etienne <etienne.vbg@hotmail.fr>
2017-04-06 16:28:10 +0200

    Add an armour class

--- objects.py
+++ objects.py
@@ -9,8 +9,9 @@ class Object():
 	def __repr__(self):
 		return self.name
 
+
 class Weapon(Object):
-	def __init__(self,name,weight,distance,damage_range,
+	def __init__(self,name,weight,distance,damage_range,style,
 				strength_min=0,agility_min=0,intelligence_min=0):
 		Object.__init__(self,name,weight)
 		self.range=distance
@@ -36,3 +37,44 @@ class Potion(Object):
 		if character.mana_points >character.max_mana_points:
 			character.mana_points=character.max_mana_points
 
+
+class Armour(Object):
+	def __init__(self,name,weight,type_of_armour,protection,style,
+					strength_min=0,endurance_min=0):
+		Object.__init__(self,name,weight)
+		self.type_of_armour=type_of_armour
+		self.protection=protection
+		self.strength_min=strength_min
+		self.endurance_min=endurance_min
+	def equip_armour(self,character):
+		if character.strength<self.strength_min:
+			return "strength too low"
+		if character.endurance<self.endurance_min:
+			return "endurance too low"
+		else:
+			if self.type_of_armour=="head":
+				old_armour=character.head_equipment
+				character.head_equipment=self
+				if old_armour!=None:
+					character.add_in_inventory(old_armour)
+			elif self.type_of_armour=="arm":
+				old_armour=character.arm_equipment
+				character.arm_equipment=self
+				if old_armour!=None:
+					character.add_in_inventory(old_armour)
+			elif self.type_of_armour=="torso":
+				old_armour=character.torso_equipment
+				character.torso_equipment=self
+				if old_armour!=None:
+					character.add_in_inventory(old_armour)
+			elif self.type_of_armour=="leg":
+				old_armour=character.leg_equipment
+				character.leg_equipment=self
+				if old_armour!=None:
+					character.add_in_inventory(old_armour)
+			elif self.type_of_armour=="foot":
+				old_armour=character.foot_equipment
+				character.foot_equipment=self
+				if old_armour!=None:
+					character.add_in_inventory(old_armour)
+			character.drop(self)
